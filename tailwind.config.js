647e6aefea30321637e6a464cf229cbdedd0daa0
Matt Styles <mattstyles333@gmail.com>
2023-02-17 12:09:01 +0000

    move files and start header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        p: "#1f2937",
+        s: "#1E3A8A",
+        l: "#d6d3d1",
+      },
+    },
   },
   plugins: [],
-}
+};
