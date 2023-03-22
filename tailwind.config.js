1edbc26aef396403de37ac96009179f41ee3e252
Matt Styles <mattstyles333@gmail.com>
2023-03-22 10:20:56 +0000

    finished email header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ["./src/email-header.html"],
   theme: {
     extend: {
       colors: {
         p: "#1f2937",
         s: "#1E3A8A",
         l: "#d6d3d1",
+        t: "#00824E",
+      },
+      screens: {
+        xxsm: "420px",
+        xxxsm: "360px",
       },
     },
   },
