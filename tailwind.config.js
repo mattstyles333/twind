51ffa706ec92ec82ec6507e3ab38c197ac3f1bd5
TannerD9 <tannerdaviesj@gmail.com>
2023-08-25 12:57:45 +0100

    lens info template

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,11 @@ module.exports = {
       colors: {
         p: "#1f2937",
         s: "#1E3A8A",
-        l: "#f9fafb",
+        l: "#f3f4f6",
         t: "#0f172a",
         a: "#f97316",
         b: "#2563eb",
+        gr: "#6b7280",
       },
       screens: {
         xxsm: "420px",
@@ -18,5 +19,4 @@ module.exports = {
     },
   },
   plugins: [],
-  
 };
