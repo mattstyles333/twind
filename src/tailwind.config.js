d2e900a2afec20fbc62023914bc73d07d2de8250
TannerD9 <tannerdaviesj@gmail.com>
2024-09-26 15:39:41 +0100

    success page - referrals

---
+++ src/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+const animations = require("@midudev/tailwind-animations");
+
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        p: "#1f2937",
+        s: "#1E3A8A",
+        l: "#f3f4f6",
+        t: "#5494EC",
+        a: "#f97316",
+        b: "#2563eb",
+        gr: "#6b7280",
+      },
+      screens: {
+        xxsm: "420px",
+        xxxsm: "360px",
+      },
+    },
+  },
+  plugins: [animations],
+};
